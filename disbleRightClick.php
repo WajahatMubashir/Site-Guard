d13f5d856fca613cd69429c8701194ca7d61e93f
Wajahat Mubashir <Wajahatmubashir85@gmail.com>
2022-06-08 14:11:48 +0500

    Feed Delay
    Feed Delay To Protect Form Bad Bots

--- disbleRightClick.php
+++ disbleRightClick.php
@@ -1,9 +1,9 @@
 <?php
 /**
-* Plugin Name: Protect_Website
+* Plugin Name: Protect-Website
 * Plugin URI: 
-* Description: A plugin to Disable Right Click, Protect Text, and Hide Source Code
-* Version: 0.0.1
+* Description: A plugin to Disable Right Click, Protect Text, Delay RSS Feed to protect from bad bots and Hide Source Code
+* Version: 0.0.3
  * Author: Wajahat Mubashir
  */
 
@@ -19,3 +19,27 @@ add_action( 'admin_enqueue_scripts', 'disableClick_load_js' );
         wp_enqueue_script('JS-load', plugin_dir_url(__FILE__).'disableClick.js');
         wp_enqueue_style( 'CSS-load', plugin_dir_url(__FILE__).'disableRightClick.css');
  }
+
+
+//Feed Delay
+ function publish_later_on_feed($where) {
+ 
+    global $wpdb;
+ 
+    if ( is_feed() ) {
+        // timestamp in WP-format
+        $now = gmdate('Y-m-d H:i:s');
+ 
+        // value for wait; + device
+        $wait = '10'; // integer
+ 
+       
+        $device = 'MINUTE'; //MINUTE, HOUR, DAY, WEEK, MONTH, YEAR
+ 
+        // add SQL-sytax to default $where
+        $where .= " AND TIMESTAMPDIFF($device, $wpdb->posts.post_date_gmt, '$now') > $wait ";
+    }
+    return $where;
+}
+ 
+add_filter('posts_where', 'publish_later_on_feed');
