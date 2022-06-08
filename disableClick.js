e638973fe02c029b5e054fad3ff2c6181585af9d
Wajahat Mubashir <Wajahatmubashir85@gmail.com>
2022-06-08 14:13:30 +0500

    Only work on logout
    Clipboard Empty itself after 1 second and left-click in allow for Admin.

--- disableClick.js
+++ disableClick.js
@@ -1,12 +1,37 @@
 /** @format */
 document.addEventListener("DOMContentLoaded", function () {
-	document.addEventListener("keydown", function (event) {
-		if (event.ctrlKey) {
-			event.preventDefault();
-		}
-	});
+	var bodyTag = document.getElementsByTagName("body");
+	var classCheck = bodyTag.classList;
+	if (jQuery("body").hasClass("logged-in")) {
+		console.log("logged-in");
+	} else {
+		document.addEventListener("keydown", function (event) {
+			if (event.ctrlKey) {
+				event.preventDefault();
+			}
+		});
 
-	document.addEventListener("contextmenu", (event) =>
-		event.preventDefault(),
-	);
+		document.addEventListener("contextmenu", (event) =>
+			event.preventDefault(),
+		);
+
+		bodyTag.setAttribute("ondragstart", "return false;");
+		bodyTag.setAttribute("onselectstart", "return false;");
+		bodyTag.setAttribute("oncontextmenu", "return false;");
+		bodyTag.setAttribute("oncopy", "return false;");
+		bodyTag.setAttribute("oncut", "return false;");
+		bodyTag.setAttribute("onpaste", "return false;");
+		bodyTag.setAttribute("onload", "clearData();");
+		bodyTag.setAttribute("onblur", "clearData();");
+
+		function clearData() {
+			window.clipboardData.setData("text", "");
+		}
+		function cldata() {
+			if (clipboardData) {
+				clipboardData.clearData();
+			}
+		}
+		setInterval("cldata();", 1000);
+	}
 });
