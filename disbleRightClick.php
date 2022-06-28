23bbeff4280bd024204f40242b13a8c982342626
Wajahat Mubashir <Wajahatmubashir85@gmail.com>
2022-06-28 13:39:58 +0500

    exit if directly accessed.

--- disbleRightClick.php
+++ disbleRightClick.php
@@ -8,7 +8,7 @@
  */
 
  // Exit if accessed directly
- if( !defined( 'ABSPATH' ) ) exit; 
+if (!defined('ABSPATH')) { exit; } 
 
  // Hook For StyleSheet
 add_action( 'wp_enqueue_scripts', 'disableClick_load_js' );
